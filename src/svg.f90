628de4eddc4a31a47e28009300d7478965c9a21c
Andre Smit <freevryheid@gmail.com>
2024-01-01 16:14:16 -0600

    init

---
+++ src/svg.f90
@@ -0,0 +1,65 @@
+module svg
+
+  use stdlib_strings, only: to_string
+  use stdlib_optval, only: optval
+
+  implicit none
+  private
+
+  integer, parameter, public :: SVG_WIDTH = 100
+  integer, parameter, public :: SVG_HEIGHT = 100
+
+  type, public :: t_svg
+    integer :: width, height
+    character(len=:), allocatable :: xml
+    contains
+      procedure :: head
+      procedure :: tail
+      procedure :: export
+      procedure :: clean
+      procedure :: add_text
+  end type
+
+  contains
+
+    subroutine head(self, width, height)
+      class(t_svg) :: self
+      integer, optional :: width, height
+      self%width = optval(width, SVG_WIDTH)
+      self%height = optval(height, SVG_HEIGHT)
+      self%xml = '<svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" viewBox="0 0 ' // &
+      & to_string(self%width) // ' ' // to_string(self%height) // '">' // new_line('a')
+    end subroutine head
+
+    subroutine tail(self)
+      class(t_svg) :: self
+      self%xml = self%xml // '</svg>'
+    end subroutine tail
+
+    subroutine export(self, fin)
+      class(t_svg) :: self
+      character(len=*), optional :: fin
+      character(len=:), allocatable :: fout
+      integer :: io
+      fout = "tst.svg"
+      fout = optval(fin, fout)
+      open(newunit=io, file=fout, status="replace", action="write")
+      write(io, '(a)') self%xml
+      close(io)
+    end subroutine export
+
+    subroutine clean(self)
+      class(t_svg) :: self
+      deallocate(self%xml)
+    end subroutine clean
+
+    subroutine add_text(self, x, y, fs, fill, txt)
+      class(t_svg) :: self
+      integer :: x, y, fs
+      character(len=*) :: fill, txt
+      self%xml = self%xml // '<text x="' // to_string(x) // '" y="' // to_string(y) // &
+      & '" font-size="' // to_string(fs) // '" fill="' // fill // '">' // &
+      & txt // '</text>' // new_line('(a)')
+    end subroutine add_text
+
+end module svg
---
+++ test/check.f90
@@ -0,0 +1,19 @@
+program check
+  use svg
+  implicit none
+  type(t_svg) s
+  integer i
+  integer a(10, 2)
+  block
+    do i = 1, 10
+      a(i, 1) = i
+      a(i, 2) = i
+    end do
+    call s%head()
+    ! call s%add_text(50, 50, 10, "red", "Hello, World!")
+    call s%tail()
+    call s%export()
+    call s%clean()
+    ! print *, s%xml
+  end block
+end program check
